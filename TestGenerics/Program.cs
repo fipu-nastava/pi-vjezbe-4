28b958385ed5379e453201d695b2105f03be803a
Nikola Tankovic <nikola.tankovic@gmail.com>
2016-10-19 14:01:59 +0200

    Ispravak

---
+++ TestGenerics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestGenerics
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double a = GenerickaKlasa<double>.kreirajInstancu();
+
+			GenerickaKlasa<double>.usporedi(1.0, 2.0);
+
+			GenerickaKlasa<int>.usporedi(1, 2);
+		}
+	}
+}
