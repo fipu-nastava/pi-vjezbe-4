348caa069a390323a8b560507f6c42e9ef91abf0
ntankovic <nikola.tankovic@gmail.com>
2017-12-19 12:02:03 +0100

    Singleton

---
+++ Singleton/NekaSingletonKlasa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Singleton
+{
+	public class NekaSingletonKlasa
+	{
+		public String naziv;
+
+		private static NekaSingletonKlasa jedinaInstanca;
+
+		private NekaSingletonKlasa()
+		{
+		}
+
+		public static NekaSingletonKlasa dajSingleton()
+		{
+
+			if (NekaSingletonKlasa.jedinaInstanca == null)
+			{
+				NekaSingletonKlasa.jedinaInstanca = new NekaSingletonKlasa();
+				return NekaSingletonKlasa.jedinaInstanca;
+			}
+			else
+			{
+				return NekaSingletonKlasa.jedinaInstanca;
+			}
+		}
+	}
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var k1 = NekaSingletonKlasa.dajSingleton();
+
+			k1.naziv = "OK";
+
+			Console.WriteLine(k1.naziv);
+
+			var k2 = NekaSingletonKlasa.dajSingleton();
+
+			Console.WriteLine(k2.naziv);
+		}
+	}
+}
