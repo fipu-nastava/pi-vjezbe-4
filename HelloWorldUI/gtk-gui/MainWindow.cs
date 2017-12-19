7dd1d56ecab99b2c74ea7ac4a347a36377ba7e73
Nikola Tanković <nikola.tankovic@gmail.com>
2017-12-19 12:09:48 +0100

    HelloWorld remake

--- HelloWorldUI/gtk-gui/MainWindow.cs
+++ HelloWorldUI/gtk-gui/MainWindow.cs
@@ -3,10 +3,6 @@
 
 public partial class MainWindow
 {
-	private global::Gtk.VBox vbox1;
-
-	private global::Gtk.Label label1;
-
 	protected virtual void Build()
 	{
 		global::Stetic.Gui.Initialize(this);
@@ -14,19 +10,6 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
-		// Container child MainWindow.Gtk.Container+ContainerChild
-		this.vbox1 = new global::Gtk.VBox();
-		this.vbox1.Name = "vbox1";
-		this.vbox1.Spacing = 6;
-		// Container child vbox1.Gtk.Box+BoxChild
-		this.label1 = new global::Gtk.Label();
-		this.label1.Name = "label1";
-		this.label1.LabelProp = global::Mono.Unix.Catalog.GetString("Hello World!");
-		this.vbox1.Add(this.label1);
-		global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.label1]));
-		w1.Position = 0;
-		w1.Fill = false;
-		this.Add(this.vbox1);
 		if ((this.Child != null))
 		{
 			this.Child.ShowAll();
